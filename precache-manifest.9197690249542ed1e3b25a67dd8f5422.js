090921705e698eff5c494950ffac8f2f64ccc8af
Mikkel <mikkel.p.chambers@gmail.com>
2019-02-25 19:44:26 -0600

    Updates

---
+++ precache-manifest.9197690249542ed1e3b25a67dd8f5422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a3f390b6f79aea90564",
+    "url": "/clickclickgame/static/js/runtime~main.7a3f390b.js"
+  },
+  {
+    "revision": "e039d0a314ebc2eb652e",
+    "url": "/clickclickgame/static/js/main.e039d0a3.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/clickclickgame/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "e039d0a314ebc2eb652e",
+    "url": "/clickclickgame/static/css/main.a574c8d6.chunk.css"
+  },
+  {
+    "revision": "f6e758c6ca3595dbdad209db2b85a008",
+    "url": "/clickclickgame/index.html"
+  }
+];
